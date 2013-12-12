679aa1415287eff8155870ac881a7f32140d83fa
Giordon Stark <kratsg@gmail.com>
2013-12-12 00:13:30 -0600

    initial commit moving into a restructured file

---
+++ root_jets.py
@@ -0,0 +1,10 @@
+''' Class definitions for dealing with ATLAS jets'''
+
+#needed to deal with rootfiles
+import ROOT
+import numpy as np
+import matplotlib.pyplot as pl
+
+class Grid:
+  __init__(self):
+    
