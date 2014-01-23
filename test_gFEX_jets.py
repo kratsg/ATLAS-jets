3092a3cfdea3ddab493b83b8264f35cb81eb2577
Giordon Stark <kratsg@gmail.com>
2014-01-23 16:02:59 -0600

    Added in seed filtering

--- test_gFEX_jets.py
+++ test_gFEX_jets.py
@@ -6,9 +6,17 @@ tEvents.load()
 print 'Loaded and built all objects for gTowers data'
 num = 0
 grids = []
+
+#this goes through and plots each event using the towers
 for tower_event in tEvents:
+  break #remoe if you want to go through loop
   grid = Grid(pixel_resolution=0.01)
   num += 1
   grid.add_tower_event(tower_event)
   grids.append(grid)
   grid.save(title='Event %d, cell resolution=0.01' % num, filename='output_event_%d.png' % num)
+
+seed_filter = SeedFilter(ETthresh = 20.0)#20.0 GeV
+for tEvent in tEvents:
+  tEvent.set_seed_filter(seed_filter)
+
