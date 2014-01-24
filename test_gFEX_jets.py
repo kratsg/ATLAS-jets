ae5adff500c1e14256f3dca21f958c914459f936
Giordon Stark <kratsg@gmail.com>
2014-01-24 16:48:17 -0600

    We went full circle with towers -> seeds -> jets. We just need to reconstitute the jets using a gaussian algorithm instead of a uniform-distribution.

--- test_gFEX_jets.py
+++ test_gFEX_jets.py
@@ -1,22 +1,20 @@
 from gFEX_jets import *
 filename = '/Users/kratsg/Dropbox/UChicagoSchool/DavidMiller/Data/PileupSkim.root'
-tEvents = TowerEvents(filename=filename)
+tEvents = TowerEvents(filename=filename, seed_filter=SeedFilter(ETthresh=20.0))
 tEvents.load()
 
 print 'Loaded and built all objects for gTowers data'
 num = 0
-grids = []
+
+domain = np.array([[-3.2,3.2],[-4.9,4.9]])
 
 #this goes through and plots each event using the towers
 for tower_event in tEvents:
-  break #remoe if you want to go through loop
-  grid = Grid(pixel_resolution=0.01)
   num += 1
-  grid.add_tower_event(tower_event)
-  grids.append(grid)
-  grid.save(title='Event %d, cell resolution=0.01' % num, filename='output_event_%d.png' % num)
+  grid_before = Grid(pixel_resolution=0.01, domain=domain)
+  grid_before.add_tower_event(tower_event)
+  grid_before.save(title='Event %d, cell resolution=0.01' % num, filename='output_tower_%d.png' % num)
 
-seed_filter = SeedFilter(ETthresh = 20.0)#20.0 GeV
-for tEvent in tEvents:
-  tEvent.set_seed_filter(seed_filter)
-
+  grid_after = Grid(pixel_resolution=0.01, recon_algo = 'gaussian', domain=domain)
+  grid_after.add_event(tower_event.get_event())
+  grid_after.save(title='Event %d, cell resolution=0.01' % num, filename='output_event_%d.png' % num)
